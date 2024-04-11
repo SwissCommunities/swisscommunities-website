8e0836d58515f8a1d755e73d6b090d0ce1e98c5f
Dråfølin <derg@drafolin.ch>
2024-04-11 17:14:53 +0200

    copyright

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,11 +1,23 @@
 <script lang="ts" setup>
-
+const currentYear = new Date().getFullYear();
 </script>
 
 <template>
-    <footer></footer>
+    <footer>
+        <!-- TODO: Social links -->
+        <div class="copy">
+            <span>&copy; 2024</span>
+            <span v-if="currentYear !== 2024">&nbsp;- {{ currentYear }}</span>
+            <span>&nbsp;<NuxtLink href="https://drafolin.ch">Dråfølin</NuxtLink></span>
+        </div>
+    </footer>
 </template>
 
 <style lang="scss" scoped>
-
+footer {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 4em;
+}
 </style>
