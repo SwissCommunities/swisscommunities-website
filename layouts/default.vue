22776c66e1ba572645da7ea85273e7faea796338
Dråfølin <derg@drafolin.ch>
2024-04-11 14:46:19 +0200

    Base Layout

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+import TheHeader from "~/components/TheHeader.vue";
+import TheFooter from "~/components/TheFooter.vue";
+</script>
+
+<template>
+    <header>
+        <TheHeader/>
+    </header>
+
+    <main>
+        <slot/>
+    </main>
+
+    <footer>
+        <TheFooter/>
+    </footer>
+</template>
+
+<style scoped>
+
+</style>
