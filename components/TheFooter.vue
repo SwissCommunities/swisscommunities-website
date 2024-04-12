149d1d083bd68f2d2e88086d9a4ac5ae5f70a7e5
Dråfølin <derg@drafolin.ch>
2024-04-12 18:52:38 +0200

    added discord link

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,10 +1,22 @@
 <script lang="ts" setup>
+import DiscordIcon from "~/assets/images/discord.svg"
+
 const currentYear = new Date().getFullYear();
 </script>
 
 <template>
     <footer>
-        <!-- TODO: Social links -->
+        <div class="social">
+            <nav>
+                <ul>
+                    <li>
+                        <NuxtLink to="https://discord.gg/tkxNt6YMBb">
+                            <DiscordIcon class="discord"/>
+                        </NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
         <div class="copy">
             <span>&copy; 2024</span>
             <span v-if="currentYear !== 2024">&nbsp;- {{ currentYear }}</span>
@@ -19,5 +31,33 @@ footer {
   align-items: center;
   flex-direction: column;
   padding: 4em;
+
+  .social {
+    nav {
+      margin-bottom: 1em;
+
+      ul {
+        li {
+          a {
+            font-size: 4rem;
+
+            svg, img {
+              height: 1em;
+              color: black;
+              transition: filter 300ms ease-in-out;
+              filter: grayscale(1);
+
+              &:hover {
+                filter: none;
+              }
+
+              @media screen and (prefers-color-scheme: dark) {
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
 </style>
