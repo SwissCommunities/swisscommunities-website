b53119d4f6a746b5c7a9809e22ed02b9ba1269da
Dråfølin <derg@drafolin.ch>
2025-01-30 22:59:08 +0100

    added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,27 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "SwissCommunities",
+      meta: [
+        {
+          charset: "utf-8",
+        },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+      ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.svg",
+        },
+      ],
+    },
+  },
   vite: {
     css: {
       preprocessorOptions: {
